2a0b2dcce6c482d942a14a1bec674f45291286bf
awolde <amangb@hotmail.com>
2025-08-27 21:27:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,9 @@ terraform {
 provider "null" {}
 
 resource "null_resource" "hello_world" {
+  triggers = {
+    requirements_sha1 = filesha1("${path.module}/requirements.txt")
+  }
   provisioner "local-exec" {
     command = <<EOT
       python3 -m pip install -r requirements.txt
