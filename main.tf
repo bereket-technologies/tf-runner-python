b7ebfa5150822a7ea856b006fe04c077b82ba132
awolde <amangb@hotmail.com>
2025-09-17 19:03:22 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
-terraform {
-  required_version = ">= 0.13"
-}
+
 
 provider "null" {}
 
@@ -14,3 +12,51 @@ resource "null_resource" "hello_world" {
     EOT
   }
 }
+
+resource "google_storage_bucket" "terraform_state" {
+  name          = "tf-runner-dev-buckket"
+  location      = "us-central1"
+  project       = "bcg-demotoo-prj-0188"
+  storage_class = "STANDARD"
+
+  # Prevent accidental deletion of the bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  # Enable versioning for state file history
+  versioning {
+    enabled = true
+  }
+
+  # Enable uniform bucket-level access for better security
+  uniform_bucket_level_access = true
+
+
+  # Lifecycle management to clean up old versions
+  lifecycle_rule {
+    condition {
+      age = 30
+      with_state = "ARCHIVED"
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      num_newer_versions = 10
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  # Labels for organization and cost tracking
+  labels = {
+    purpose     = "terraform-state"
+    managed_by  = "terraform"
+  }
+}
+
